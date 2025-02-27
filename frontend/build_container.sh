0520894ad39b63cfbe15fa3a8c7d9932b10ec1c2
Felix Niedermaier <felix.niedermaier@univie.ac.at>
2025-02-27 16:51:36 +0100

    update container build for frontend

--- frontend/build_container.sh
+++ frontend/build_container.sh
@@ -3,9 +3,9 @@
 echo "Building lipidlibrarianweb_frontend OCI container..."
 
 buildah build --layers -f Containerfile \
-  --build-arg FRONTEND_PORT=${FRONTEND_PORT} \
-  --build-arg ANGULAR_BUILD_CONFIGURATION=${ANGULAR_BUILD_CONFIGURATION} \
-  --build-arg APP_URL_DEST=${APP_URL_DEST} \
+  --build-arg FRONTEND_PORT="${FRONTEND_PORT}" \
+  --build-arg ANGULAR_BUILD_CONFIGURATION="${ANGULAR_BUILD_CONFIGURATION}" \
+  --build-arg APP_URL_DEST="${APP_URL_DEST}" \
   -t lipidlibrarianweb_frontend .
 
 echo "Building lipidlibrarianweb_frontend OCI container done."
