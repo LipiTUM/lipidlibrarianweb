0293eb353f3e71a3065e8e48fcf248b744b8b0ed
Felix Niedermaier <felix.niedermaier@univie.ac.at>
2025-02-28 10:46:43 +0100

    update container build for frontend and backend

--- backend/container-entrypoint.sh
+++ backend/container-entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Wait for the database
 echo "Waiting for postgres..."
-while ! nc -z ${DJANGO_DB_HOST} ${DJANGO_DB_PORT}; do
+while ! nc -z ${DJANGO_DB_HOST:=localhost} ${DJANGO_DB_PORT:=5432}; do
 	sleep 0.1
 done
 echo "PostgreSQL started"
@@ -11,4 +11,4 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --no-input --clear
 
-gunicorn lipidlibrarianweb.wsgi:application --bind 0.0.0.0:${BACKEND_PORT} --timeout 480 --workers 12 --user=root --group=root
+gunicorn lipidlibrarianweb.wsgi:application --bind 0.0.0.0:${BACKEND_PORT:=81} --timeout 480 --workers 12 --user=root --group=root
--- frontend/build_container.sh
+++ frontend/build_container.sh
@@ -3,7 +3,6 @@
 echo "Building lipidlibrarianweb_frontend OCI container..."
 
 buildah build --layers -f Containerfile \
-  --build-arg FRONTEND_PORT="${FRONTEND_PORT}" \
   --build-arg ANGULAR_BUILD_CONFIGURATION="${ANGULAR_BUILD_CONFIGURATION}" \
   --build-arg APP_URL_DEST="${APP_URL_DEST}" \
   -t lipidlibrarianweb_frontend .
