c9de1208d19e1319200114187197a0a6e9a343a2
Felix Niedermaier <felix.niedermaier@univie.ac.at>
2025-03-05 18:17:07 +0100

    WIP: ontology graph

--- frontend/src/app/models/ontology-graph-edge.model.ts
+++ frontend/src/app/models/ontology-graph-edge.model.ts
@@ -2,12 +2,14 @@ import { OntologyGraphNode } from "./ontology-graph-node.model";
 
 
 export class OntologyGraphEdge implements d3.SimulationLinkDatum<OntologyGraphNode> {
+  id!: string;
   source!: OntologyGraphNode;
   target!: OntologyGraphNode;
   label?: string;
 
   constructor(data?: any) {
     if (data) {
+      this.id = data.source.id + '-' + data.target.id;
       this.source = data.source;
       this.target = data.target;
       if (data.label) {
