55e63ce967fbc6947202395a52fa1f6bedfe69bd
Felix Niedermaier <felix.niedermaier@univie.ac.at>
2025-03-05 18:17:07 +0100

    WIP: ontology graph

--- frontend/src/app/components/ontology/ontology-graph.component.ts
+++ frontend/src/app/components/ontology/ontology-graph.component.ts
@@ -1,26 +1,19 @@
 import { AfterViewInit, Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { NgFor } from '@angular/common';
 import { Observable } from 'rxjs';
+import { Edge, NgxGraphModule, Node } from '@swimlane/ngx-graph';
 
 import { Lipid } from 'src/app/models/lipid.model';
 import { OntologyGraph } from 'src/app/models/ontology-graph.model';
 import { OntologyGraphEdge } from 'src/app/models/ontology-graph-edge.model';
 import { OntologyGraphNode } from 'src/app/models/ontology-graph-node.model';
-import { OntologyGraphService } from 'src/app/services/ontology-graph.service';
-import { OntologyGraphDraggableBehaviourDirective } from 'src/app/directives/ontology-graph-draggable-behaviour.directive';
-import { OntologyGraphZoomableBehaviourDirective } from 'src/app/directives/ontology-graph-zoomable-behaviour.directive';
-import { OntologyGraphNodeComponent } from './ontology-graph-node.component';
-import { OntologyGraphEdgeComponent } from './ontology-graph-edge.component';
 
 
 @Component({
     selector: 'app-ontology-graph',
     imports: [
         NgFor,
-        OntologyGraphNodeComponent,
-        OntologyGraphEdgeComponent,
-        OntologyGraphDraggableBehaviourDirective,
-        OntologyGraphZoomableBehaviourDirective,
+        NgxGraphModule,
     ],
     templateUrl: './ontology-graph.component.html',
     styleUrls: ['./ontology-graph.component.sass']
@@ -28,47 +21,43 @@ import { OntologyGraphEdgeComponent } from './ontology-graph-edge.component';
 export class OntologyGraphComponent implements AfterViewInit, OnChanges {
   @Input() lipid$?: Observable<Lipid | undefined>;
   graph?: OntologyGraph;
-  edges?: Array<OntologyGraphEdge>;
-  nodes?: Array<OntologyGraphNode>;
+  edges!: Array<Edge>;
+  nodes!: Array<Node>;
 
   _options: { width: number, height: number } = { width: 800, height: 600 };
 
-  constructor(private ontologyGraphService: OntologyGraphService) { }
+  constructor() { }
 
   get options() {
     return this._options = {
-      width: window.innerWidth,
-      height: window.innerHeight
+      width: 800,
+      height: 600
     };
   }
 
   ngAfterViewInit(): void {
     this.lipid$!.subscribe(lipid => {
       if (lipid && lipid.ontology && lipid.ontology.nodes) {
-        this.edges = lipid.ontology.edges;
-        this.nodes = lipid.ontology.nodes;
+        this.edges = lipid.ontology.edges! as unknown as Array<Edge>;
+        this.nodes = lipid.ontology.nodes! as unknown as Array<Node>;
       } else {
         this.edges = [];
         this.nodes = [];
       }
-      this.graph = this.ontologyGraphService.getOntologyGraph(this.nodes, this.edges, this.options);
-      console.log("Ontology Graph: " + JSON.stringify(this.graph))
-      this.graph.initSimulation(this.options);
+      console.log("Ontology Graph: initialized for lipid " + JSON.stringify(lipid?.nomenclature.name))
     });
   }
 
   ngOnChanges(changes: SimpleChanges): void {
     this.lipid$!.subscribe(lipid => {
       if (lipid && lipid.ontology && lipid.ontology.nodes) {
-        this.edges = lipid.ontology.edges;
-        this.nodes = lipid.ontology.nodes;
+        this.edges = lipid.ontology.edges! as unknown as Array<Edge>;
+        this.nodes = lipid.ontology.nodes! as unknown as Array<Node>;
       } else {
         this.edges = [];
         this.nodes = [];
       }
-      this.graph = this.ontologyGraphService.getOntologyGraph(this.nodes, this.edges, this.options);
-      console.log("Ontology Graph: " + JSON.stringify(this.graph))
-      this.graph.initSimulation(this.options);
+      console.log("Ontology Graph: updated for lipid " + JSON.stringify(lipid?.nomenclature.name))
     });
   }
 }
