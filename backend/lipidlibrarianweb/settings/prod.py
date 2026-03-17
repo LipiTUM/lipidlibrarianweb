e5401d51d7ccd595dfdd7ef99308aa7cd9a7561c
Felix Niedermaier <felix.niedermaier@univie.ac.at>
2026-03-17 19:50:12 +0100

    fix oom errors due to non-recycled worker threads

--- backend/lipidlibrarianweb/settings/prod.py
+++ backend/lipidlibrarianweb/settings/prod.py
@@ -65,6 +65,10 @@ Q_CLUSTER = {
     'redis': f'redis://{os.environ.get("DJANGO_CACHE_HOST")}:{os.environ.get("DJANGO_CACHE_PORT")}',
     'retry': 180,
     'timeout': 120,
+    'save_limit': 0,
+    'orm': 'default',
+    'recycle': 1,
+    'poll': 2,
 }
 
 # Static files (CSS, JavaScript, Images)
